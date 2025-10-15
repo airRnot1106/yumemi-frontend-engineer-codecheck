c360e268ecd9006724b76b1016988240843935a1
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-10-15 20:59:20 +0900

    chore: update next settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
