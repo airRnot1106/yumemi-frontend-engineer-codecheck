375a6cc67bce88e89ac49e0a26e1375892c41b60
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-10-15 21:03:08 +0900

    chore: add custom color and font size tokens

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,41 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          base: { value: 'var(--colors-base)' },
+          surface: { value: 'var(--colors-surface)' },
+          overlay: { value: 'var(--colors-overlay)' },
+          muted: { value: 'var(--colors-muted)' },
+          subtle: { value: 'var(--colors-subtle)' },
+          text: { value: 'var(--colors-text)' },
+          love: { value: 'var(--colors-love)' },
+          gold: { value: 'var(--colors-gold)' },
+          rose: { value: 'var(--colors-rose)' },
+          pine: { value: 'var(--colors-pine)' },
+          foam: { value: 'var(--colors-foam)' },
+          iris: { value: 'var(--colors-iris)' },
+          'highlight-low': { value: 'var(--colors-highlight-low)' },
+          'highlight-med': { value: 'var(--colors-highlight-med)' },
+          'highlight-high': { value: 'var(--colors-highlight-high)' },
+        },
+        fontSizes: {
+          xs: { value: 'var(--font-sizes-xs)' },
+          sm: { value: 'var(--font-sizes-sm)' },
+          md: { value: 'var(--font-sizes-md)' },
+          lg: { value: 'var(--font-sizes-lg)' },
+          xl: { value: 'var(--font-sizes-xl)' },
+          '2xl': { value: 'var(--font-sizes-2xl)' },
+          '3xl': { value: 'var(--font-sizes-3xl)' },
+          '4xl': { value: 'var(--font-sizes-4xl)' },
+          '5xl': { value: 'var(--font-sizes-5xl)' },
+          '6xl': { value: 'var(--font-sizes-6xl)' },
+          '7xl': { value: 'var(--font-sizes-7xl)' },
+          '8xl': { value: 'var(--font-sizes-8xl)' },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
