77066cfcc3e9a961616981b0c23689f2e5e27aa0
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-10-15 21:23:38 +0900

    chore: add vitest project for unit test

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,6 +14,15 @@ export default defineConfig({
     projects: [
       {
         extends: true,
+        test: {
+          name: 'unit',
+          environment: 'jsdom',
+          include: ['**/*.test.ts', '**/*.test.tsx'],
+          includeSource: ['src/**/*.{ts,tsx}'],
+        },
+      },
+      {
+        extends: true,
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
