32f2f429240300c3d865a609e7a44e911950d023
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-10-25 14:11:33 +0900

    feat: add prefecture-code model

---
+++ src/features/prefecture/models/prefecture-code.ts
@@ -0,0 +1,84 @@
+import { R } from '@praha/byethrow';
+import { ErrorFactory } from '@praha/error-factory';
+import z from 'zod';
+
+declare const PrefectureCodeBrand: unique symbol;
+
+const PrefectureCodeSchemaWithoutBrand = z.number().int();
+const PrefectureCodeSchema =
+  PrefectureCodeSchemaWithoutBrand.brand<typeof PrefectureCodeBrand>();
+
+type PrefectureCodeWithoutBrand = z.infer<
+  typeof PrefectureCodeSchemaWithoutBrand
+>;
+export type PrefectureCode = z.infer<typeof PrefectureCodeSchema>;
+
+export class InvalidPrefectureCodeError extends ErrorFactory({
+  name: 'InvalidPrefectureCodeError',
+  message: ({ prefectureCode }) => `Invalid PrefectureCode: ${prefectureCode}`,
+  fields: ErrorFactory.fields<{ prefectureCode: PrefectureCodeWithoutBrand }>(),
+}) {}
+
+const create = (prefectureCode: PrefectureCodeWithoutBrand) =>
+  R.pipe(
+    R.succeed(prefectureCode),
+    R.andThen(R.parse(PrefectureCodeSchema)),
+    R.mapError(() => new InvalidPrefectureCodeError({ prefectureCode })),
+  );
+
+export const PrefectureCode = {
+  schema: PrefectureCodeSchema,
+  withoutBrandSchema: PrefectureCodeSchemaWithoutBrand,
+  create,
+};
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+  const fc = await import('fast-check');
+
+  describe('PrefectureCodes', () => {
+    describe('create', () => {
+      it('should succeed for integers', () => {
+        fc.assert(
+          fc.property(fc.integer(), (value) => {
+            const result = PrefectureCode.create(value);
+            expect(R.isSuccess(result)).toBe(true);
+          }),
+        );
+      });
+
+      it('should fail when given a float number', () => {
+        fc.assert(
+          fc.property(
+            fc
+              .float()
+              .filter((n) => !Number.isInteger(n) && Number.isFinite(n)),
+            (value) => {
+              const result = PrefectureCode.create(value);
+              expect(R.isFailure(result)).toBe(true);
+              if (R.isFailure(result)) {
+                expect(result.error).toBeInstanceOf(InvalidPrefectureCodeError);
+              }
+            },
+          ),
+        );
+      });
+
+      it('should fail when given NaN', () => {
+        const result = PrefectureCode.create(NaN);
+        expect(R.isFailure(result)).toBe(true);
+        if (R.isFailure(result)) {
+          expect(result.error).toBeInstanceOf(InvalidPrefectureCodeError);
+        }
+      });
+
+      it('should fail when given Infinity', () => {
+        const result = PrefectureCode.create(Infinity);
+        expect(R.isFailure(result)).toBe(true);
+        if (R.isFailure(result)) {
+          expect(result.error).toBeInstanceOf(InvalidPrefectureCodeError);
+        }
+      });
+    });
+  });
+}
