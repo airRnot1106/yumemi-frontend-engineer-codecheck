67fbecd1e5582cf01ade41de4a2139d86a478c90
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2025-10-27 21:39:48 +0900

    feat: update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,41 @@
-import Image from 'next/image';
+import { Suspense } from 'react';
+import { css } from '../../styled-system/css';
+import { LoadingIndicator } from '../components/indicator';
+import { PopulationCompositionSectionContainer } from '../features/population-composition/components/population-composition-section';
 
-export default function Home() {
+export default async function Home() {
   return (
-    <div>
-      <main>
-        <Image
-          alt="Next.js logo"
-          height={38}
-          priority
-          src="/next.svg"
-          width={180}
+    <div
+      className={css({
+        containerType: 'inline-size',
+        height: 'full',
+      })}
+    >
+      <Suspense
+        fallback={
+          <div
+            className={css({
+              display: 'grid',
+              placeContent: 'center',
+              width: 'full',
+              height: 'full',
+            })}
+          >
+            <LoadingIndicator variant="spinner" />
+          </div>
+        }
+      >
+        <PopulationCompositionSectionContainer
+          className={css({
+            margin: {
+              base: '2',
+              '@/sm': '4',
+              '@/md': '6',
+              '@/lg': '8',
+            },
+          })}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div>
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <Image
-              alt="Vercel logomark"
-              height={20}
-              src="/vercel.svg"
-              width={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            alt="File icon"
-            aria-hidden
-            height={16}
-            src="/file.svg"
-            width={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            alt="Window icon"
-            aria-hidden
-            height={16}
-            src="/window.svg"
-            width={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            alt="Globe icon"
-            aria-hidden
-            height={16}
-            src="/globe.svg"
-            width={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </Suspense>
     </div>
   );
 }
